dede90a60d210525882d7017c2df691ad8da6d58
Kipeles Kemboi <47624821+deniskipeles@users.noreply.github.com>
2024-06-16 20:50:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
 from flask import Flask, jsonify
 import threading
 import logging
-from processor import consumer, consumer_running
+import requests
+from processor import consumer, scrape_data
+from fetcher import fetch_and_cache
 
 app = Flask(__name__)
 
-# Global variables to track consumer status
-consumer_thread = None
-
-@app.route('/start-consumer', methods=['POST'])
-def start_consumer():
-    global consumer_thread, consumer_running
-    
-    if consumer_running:
-        return jsonify({"status": "Consumer is already running"}), 200
-
-    consumer_thread = threading.Thread(target=run_consumer)
-    consumer_thread.start()
-    consumer_running = True
-
-    return jsonify({"status": "Consumer started"}), 200
+# Global variable to track consumer status
+consumer_running = False
 
 def run_consumer():
     global consumer_running
-
     try:
         consumer_running = True
         consumer()
@@ -32,20 +20,44 @@ def run_consumer():
     finally:
         consumer_running = False
 
+@app.route('/start-consumer', methods=['POST'])
+def start_consumer():
+    global consumer_running
+
+    if consumer_running:
+        return jsonify({"status": "Consumer is already running"}), 200
+
+    consumer_thread = threading.Thread(target=run_consumer)
+    consumer_thread.start()
+    return jsonify({"status": "Consumer starting"}), 200
+
 @app.route('/stop-consumer', methods=['POST'])
 def stop_consumer():
-    global consumer_thread, consumer_running
+    global consumer_running
 
     if not consumer_running:
         return jsonify({"status": "Consumer is not running"}), 200
 
     consumer_running = False
-    if consumer_thread is not None:
-        consumer_thread.join()
-        consumer_thread = None
+    return jsonify({"status": "Consumer stopping"}), 200
 
-    return jsonify({"status": "Consumer stopped"}), 200
+def run_scan_and_consumer():
+    # Initiate scraping process
+    url = "https://stories-blog.pockethost.io/api/collections/scraper_controllers/records"
+    data = fetch_and_cache(url)
+    if data:
+        for scraper_config in data['items']:
+            print(scraper_config)
+            scrape_data(scraper_config)
 
+    # Start the consumer after scraping is complete
+    run_consumer()
+
+@app.route('/scan', methods=['POST'])
+def scan_and_start_consumer():
+    scan_thread = threading.Thread(target=run_scan_and_consumer)
+    scan_thread.start()
+    return jsonify({"status": "Scan initiated and consumer will start"}), 200
 
 @app.route('/')
 def hello_world():

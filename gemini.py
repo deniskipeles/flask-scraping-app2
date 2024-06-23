c1084cb5dc0e23515e6b95ea07176a59d3a175f6
Kipeles Kemboi <47624821+deniskipeles@users.noreply.github.com>
2024-06-23 17:44:07 +0300

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -1,6 +1,14 @@
 import requests
 import json
 
+def get_text(json_data):
+    text = None
+    try:
+        text = json_data["candidates"][0]["content"]["parts"][0]["text"]
+    except (KeyError, IndexError):
+        pass
+    return text
+    
 def gemini_generate_content(api_key, text):
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={api_key}"
     data = {
@@ -12,7 +20,8 @@ def gemini_generate_content(api_key, text):
     }
     response = requests.post(url, headers={"Content-Type": "application/json"}, json=data)
     if response.status_code == 200:
-        return response.json()
+        json_data = response.json()
+        return get_text(json_data)
     else:
         return None
 

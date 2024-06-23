e68af045052f2a437c3825d527f8f22cf1bb4566
Kipeles Kemboi <47624821+deniskipeles@users.noreply.github.com>
2024-06-23 16:56:29 +0300

    Create gemini.py

---
+++ gemini.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+def generate_content(api_key, text):
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={api_key}"
+    data = {
+        "contents": [{
+            "parts": [{
+                "text": text
+            }]
+        }]
+    }
+    response = requests.post(url, headers={"Content-Type": "application/json"}, json=data)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+

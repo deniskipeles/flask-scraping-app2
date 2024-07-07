c35e4c370e6d6c2b24dd3f6d356d3660f330f461
Kipeles Kemboi <47624821+deniskipeles@users.noreply.github.com>
2024-07-07 15:57:49 +0300

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -6,6 +6,11 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from config import redis_client, REDIS_CACHE_EXPIRATION
 
+import urllib.parse
+import random
+from cachetools import TTLCache
+
+
 def fetch_and_cache(url, cache_expiration=REDIS_CACHE_EXPIRATION):
     cache_key = f"cache:{url}"
     cached_value = redis_client.get(cache_key)
@@ -59,3 +64,48 @@ def find_elements(soup, selectors):
                 new_elements.extend(elem.find_all(tag) if elem else [])
         elements = new_elements
     return elements
+
+
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+cache = TTLCache(maxsize=128, ttl=300)  # 300 seconds = 5 minutes
+
+def get_tags(tags, base_url):
+    encoded_url = urllib.parse.quote_plus(str(tags))
+
+    def fetch_tags():
+        print (f"Cache miss: {tags}")
+        url = f"{base_url}/api/collections/view_articles_list/records"
+        filter_str = " || ".join(f"tags ?~ \"{tag}\"" for tag in tags)
+        filter_str = f"({filter_str})"
+        params = {
+            "page": 1,
+            "perPage": 50,
+            "filter": filter_str,
+            "sort": "-created",
+            "fields": "tags"
+        }
+        encoded_params = urllib.parse.urlencode(params, safe='()')
+        link = f"{url}?{encoded_params}"
+        response = requests.get(link)
+        if response.status_code == 200:
+            json_obj = response.json()
+            tags_list = []
+            for item in json_obj.get("items", []):
+                tags_list.append(item.get("tags", []))
+            random.shuffle(tags_list)
+            logging.debug(f"Cache hit: {tags}")
+            return tags_list if len(tags_list)>0 else [[tags]]  # return default pass tags if tags_list is empty
+        else:
+            return "Error:", response.status_code
+
+    if encoded_url in cache:
+        return cache[encoded_url]
+    else:
+        result = fetch_tags()
+        cache[encoded_url] = result
+        return result
+
+
